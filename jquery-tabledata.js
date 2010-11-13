1979cae9f89a43e7d28719a2621ede4636a629cf
Nahuel <nahuel.defosse@gmail.com>
2010-11-13 17:48:16 -0300

    Hash persistant

--- jquery-tabledata.js
+++ jquery-tabledata.js
@@ -8,9 +8,10 @@
  */
 (function ( $ ) {
 
+    // Taken from 
     var _oldShow = $.fn.show;
     var _oldHide = $.fn.hide;
-
+    
     $.fn.show = function(speed, oldCallback) {
         return $(this).each(function() {
             var obj = $(this);
@@ -42,29 +43,38 @@
             _oldHide.apply(obj, [speed, newCallback]);
         });
     }
-    
+
+
+        
+
     var methods = {
         clickHandler: function (event, cfg){
-            event.preventDefault();
+            try {
+                event.preventDefault();
+            } catch (e){
+                // Called without event
+            }
             var $rolldown = $(this).data('__rolldown');
+            
             var $link = $(this);
             
             if (! $rolldown){
                 console.log("Tabledata::Create");
                 $rolldown = methods.createElement.call(this, cfg);
                 // We bind the events here so we can have cfg references
+                var $cell = $rolldown.find('td');
                 $rolldown.bind({
                     beforeShow: function (evt){
-                        methods.beforeShow.call($rolldown, evt, $link, cfg);
+                        methods.beforeShow.call($cell, evt, $link, cfg);
                     },
                     afterShow: function (evt) {
-                        methods.afterShow.call($rolldown, evt, $link, cfg);
+                        methods.afterShow.call($cell, evt, $link, cfg);
                     },
                     beforeHide: function (evt) {
-                        methods.beforeHide.call($rolldown, evt, $link, cfg);
+                        methods.beforeHide.call($cell, evt, $link, cfg);
                     },
                     afterHide: function (evt) {
-                        methods.afterHide.call($rolldown, evt, $link, cfg);
+                        methods.afterHide.call($cell, evt, $link, cfg);
                     }
                 });
 
@@ -89,20 +99,23 @@
          * Called before.
          */
         beforeShow: function (evt, $link, cfg){
-            console.info("Before show");
-            var $cell = $(this).find('td');
             if (cfg.textHidden){
                 $link.text(cfg.textHidden);
             }
             if ($.isFunction(cfg.content)){
-                console.log("Callback for", this, arguments);
-                $cell.text(cfg.content.call(this, $link, cfg));
+                //console.log("Callback for", this, arguments);
+                $(this).html(cfg.content.call(this, $link, cfg));
             } else {
-                $cell.text(cfg.content);
+                // console.log("Content", cfg.content);
+                
+                $(this).html(cfg.content);
             }
             // Finally invoke the callback
             if ($.isFunction(cfg.callbackShow)){
                 cfg.callbackShow.apply(this, $link, cfg);
+            }
+            if (cfg.hashPersistant){
+                methods.set($link.data('__id'), 1);
             }
         },
         afterShow: function (evt, $link, cfg) {
@@ -112,6 +125,9 @@
             console.info("Before hide");
             if (cfg.textShown){
                 $link.text(cfg.textShown);
+            }
+            if (cfg.hashPersistant){
+                methods.set($link.data('__id'), 0);
             }
         },
         afterHide: function (evt, $link, cfg) {
@@ -127,9 +143,74 @@
             var next_row = $(row).next();
             $(this).data('__rolldown', next_row);
             return next_row;
+        },
+        // Hash handling taken form http://stackoverflow.com/questions/1236217/storing-data-in-uris-after-hash
+        // copyright Imagist 
+        dump : function() {
+            var hash = document.location.hash;
+            var dump = new Array();
+            
+            if(hash.length == 0) return dump;
+ 
+                hash = hash.substring(1).split('&');
+                for(var key in hash) {
+                    var pair = hash[key].split('=');
+     
+                    if(pair.length != 2 || pair[0] in dump)
+                        return undefined;
+     
+                    // escape for storage
+                    dump[unescape(pair[0])] = unescape(pair[1]);
+                }
+ 
+                return dump;
+        },
+        
+        /**
+         * Takes an associative array and stores it in the URI as a hash after the # prefix, replacing any pre-
+         * existing hash.
+         */
+        load : function(array)
+        {
+            var first = true;
+            var hash = '';
+            
+            for(var key in array)
+            {
+                if(!first) hash += '&';
+                hash += escape(key) + '=' + escape(array[key]);
+            }
+            
+            document.location.hash = hash;
+        },
+        
+        /**
+         * Get the value of a key from the hash.  If the hash does not contain the key or the hash is invalid,
+         * the function returns undefined.
+         */
+        get : function(key)
+        {
+            return this.dump()[key];
+        },
+        
+        /**
+         * Set the value of a key in the hash.  If the key does not exist, the key/value pair is added.
+         */
+        set : function(key,value)
+        {
+            var dump = this.dump();
+            dump[key] = value;
+            
+            var hash = new Array();
+            
+            for(var key in dump)
+                hash.push(escape(key) + '=' + escape(dump[key]));
+            
+            document.location.hash = hash.join('&');
         }
     }; // Methods
 
+    var counter = 0;
 
     $.fn.cellData = function (cfg){
         var default_cfg = {
@@ -165,7 +246,18 @@
             $this.click(function (event){
                 methods.clickHandler.call($this, event, cfg);
             });
+            
+            var _id = 'td_'+counter;
+            var x = $this.data('__id', _id);
+            
+            if (cfg.hashPersistant) {
+                // Handle hash 
+                if (parseInt(methods.get(_id))){
+                    methods.clickHandler.call($this, null, cfg);
+                }
+            }
 
+            counter++;
 
         });
         return this; // Chaining
