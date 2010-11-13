dbb4a6243eb3f461609b6ea13ab64983a10dcb71
Nahuel <nahuel.defosse@gmail.com>
2010-11-13 12:38:01 -0300

    Some testing with custom event dispatchers

--- jquery-tabledata.js
+++ jquery-tabledata.js
@@ -6,20 +6,44 @@
     var methods = {
         clickHandler: function (event, cfg){
             event.preventDefault();
-            if (! $(this).data('rolldown')){
-                var row = $(this).parents('tr').get(0);
-                var columns = $(row).find('td').length;
-                var x = $(row).after('<tr style="display: none;"><td colspan='+columns+'>'+cfg.content+'</td></tr>');
-                var next_row = $(row).next();
-                $(next_row).fadeIn();
-                $(this).data('rolldown', next_row);
+            var $rolldown = $(this).data('__rolldown');
+            
+            if (! $rolldown){
+                console.log("Tabledata::Create");
+                methods.createElement.call(this, cfg).fadeIn();
+                
             } else {
-                $(this).data('rolldown').toggle();
+                // Hidden
+                if ($rolldown.css('display') == 'none') {
+                    // To be showed
+                    //console.info("To be shown");
+                    
+                } else {
+                    // To be hidden
+                    //console.info("To be hidden");
+                    if (cfg.textHidden) {
+                        $(this).innerHTML = cfg.textHidden;
+                    }
+                }
+                // Toggle widget visibility
+                $rolldown.toggle();
+                //console.info("Toggle");
             }
             // Class toggle
             if (cfg.toggleClass) {
                 $(this).toggleClass(cfg.toggleClass);
             }
+        },
+        /**
+         * Creates the element
+         */
+        createElement: function () {
+            var row = $(this).parents('tr').get(0);
+            var columns = $(row).find('td').length;
+            var x = $(row).after('<tr style="display: none;"><td colspan='+columns+'></td></tr>');
+            var next_row = $(row).next();
+            $(this).data('__rolldown', next_row);
+            return next_row;
         }
     }; // Methods
 
@@ -28,31 +52,31 @@
         var default_cfg = {
             url: null,
             refreshOnShow: false,
-            content: '', // Content or URL, URL has precedense
+            content: '&nbsp', // a string or function
             callbackShow: null, // TODO: Implement this!
             callbackHide: null,  // TODO: Implement this!
             toggleClass: null,
             textShown: null,
-            textHidden: null
+            textHidden: null,
+            hashPersistant: false // Sotre state in url's hash
         };
 
 
         return this.each(function (){
+            
             if ( cfg ) {
-                $.extend( default_cfg, cfg );
+                $.extend( default_cfg, cfg);
             }
+            
             var $this = $(this);
-
-            //                     $this.click(methods.clickHandler);
+            
             if (!cfg.textShown) {
+                // Save the text show in case it has not been defined
                 cfg.textShown = $this.innerHTML;
             }
             
             $this.click(function (event){
-                //try {
-                    var self = $this;
-                    methods.clickHandler.call(self, event, cfg);
-                    //} catch (e) {alert(e);}
+                methods.clickHandler.call($this, event, cfg);
             });
 
 
