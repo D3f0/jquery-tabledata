e6a1bd6667ab793a149f073779aca54e69ce9402
Nahuel <nahuel.defosse@gmail.com>
2010-11-13 13:18:42 -0300

    Better support for listeners

--- jquery-tabledata.js
+++ jquery-tabledata.js
@@ -1,38 +1,107 @@
 /**
  * Table data plugin
+ * 
+ * Author: Nahuel Defossé
+ * Email: nahuel (dot) defosse (at) gmail
+ * Date: 2010-11-13
+ * Version: 0.1
  */
 (function ( $ ) {
 
+    var _oldShow = $.fn.show;
+    var _oldHide = $.fn.hide;
+
+    $.fn.show = function(speed, oldCallback) {
+        return $(this).each(function() {
+            var obj = $(this);
+            function newCallback () {
+                if ($.isFunction(oldCallback)) {
+                    oldCallback.apply(obj);
+                }
+                obj.trigger('afterShow');
+                
+            };
+            // you can trigger a before show if you want
+            obj.trigger('beforeShow');
+            // now use the old function to show the element passing the new callback
+            _oldShow.apply(obj, [speed, newCallback]);
+        });
+    }
+    $.fn.hide = function(speed, oldCallback) {
+        return $(this).each(function() {
+            var obj = $(this);
+            function newCallback() {
+                if ($.isFunction(oldCallback)) {
+                    oldCallback.apply(obj);
+                }
+                obj.trigger('afterHide');
+            };
+            // you can trigger a before show if you want
+            obj.trigger('beforeHide');
+            // now use the old function to show the element passing the new callback
+            _oldHide.apply(obj, [speed, newCallback]);
+        });
+    }
+    
     var methods = {
         clickHandler: function (event, cfg){
             event.preventDefault();
             var $rolldown = $(this).data('__rolldown');
+            var $link = $(this);
             
             if (! $rolldown){
                 console.log("Tabledata::Create");
-                methods.createElement.call(this, cfg).fadeIn();
+                $rolldown = methods.createElement.call(this, cfg);
+                // We bind the events here so we can have cfg references
+                $rolldown.bind({
+                    beforeShow: function (evt){
+                        methods.beforeShow.call($rolldown, evt, $link, cfg);
+                    },
+                    afterShow: function (evt) {
+                        methods.afterShow.call($rolldown, evt, $link, cfg);
+                    },
+                    beforeHide: function (evt) {
+                        methods.beforeHide.call($rolldown, evt, $link, cfg);
+                    },
+                    afterHide: function (evt) {
+                        methods.afterHide.call($rolldown, evt, $link, cfg);
+                    }
+                });
+
+
+                $rolldown.fadeIn({
+                    callback: function (evt) {
+                        methods.beforeShow.call($rolldown, evt, $link, cfg);
+                    }
+                });
                 
             } else {
-                // Hidden
-                if ($rolldown.css('display') == 'none') {
-                    // To be showed
-                    //console.info("To be shown");
-                    
-                } else {
-                    // To be hidden
-                    //console.info("To be hidden");
-                    if (cfg.textHidden) {
-                        $(this).innerHTML = cfg.textHidden;
-                    }
-                }
                 // Toggle widget visibility
                 $rolldown.toggle();
-                //console.info("Toggle");
+                console.info("Toggle");
             }
             // Class toggle
             if (cfg.toggleClass) {
                 $(this).toggleClass(cfg.toggleClass);
             }
+        },
+        beforeShow: function (evt, $link, cfg){
+            console.info("Before show");
+            if (cfg.textHidden){
+                $link.text(cfg.textHidden);
+            }
+        },
+        afterShow: function (evt, $link, cfg) {
+            console.info("After show");
+        },
+        beforeHide: function (evt, $link, cfg) {
+            console.info("Before hide");
+            if (cfg.textShown){
+                $link.text(cfg.textShown);
+            }
+        },
+        afterHide: function (evt, $link, cfg) {
+            console.info("After hide1");
         },
         /**
          * Creates the element
@@ -53,9 +122,12 @@
             url: null,
             refreshOnShow: false,
             content: '&nbsp', // a string or function
+            
             callbackShow: null, // TODO: Implement this!
             callbackHide: null,  // TODO: Implement this!
+
             toggleClass: null,
+            
             textShown: null,
             textHidden: null,
             hashPersistant: false // Sotre state in url's hash
@@ -64,15 +136,17 @@
 
         return this.each(function (){
             
-            if ( cfg ) {
-                $.extend( default_cfg, cfg);
-            }
-            
+//             if ( cfg ) {
+//                 $.extend( default_cfg, cfg);
+//             }
+            cfg = $.extend({}, default_cfg, cfg);
+
+            console.log("CFG:", cfg);
             var $this = $(this);
             
             if (!cfg.textShown) {
                 // Save the text show in case it has not been defined
-                cfg.textShown = $this.innerHTML;
+                cfg.textShown = $this.text();
             }
             
             $this.click(function (event){
@@ -81,6 +155,6 @@
 
 
         });
-        return this; // Chainging
+        return this; // Chaining
     }
 })( jQuery );
